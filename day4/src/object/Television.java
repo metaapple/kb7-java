c4744a629da5205736aa9d5b78b95e622c5e0f7d
metaapple <hera@naver.com>
2026-04-20 10:55:13 +0900

    object

--- day4/src/object/Television.java
+++ day4/src/object/Television.java
@@ -1,4 +1,5 @@
 package object;
+
 public class Television {
     //우리 클래스 하나 만들어보자.
     //특성을 찾아서 클래스를 만들어야함.
@@ -19,7 +20,7 @@ public class Television {
     //메서드로 만들어 놓으면 됨.
     //멤버메서드라고 함.
     //끄다, 켜다만 선택함.
-    public void powerOn(){
+    public void powerOn() {
         int bonus = 10000; //지역변수
         System.out.println("tv를 켜다.");
         System.out.println(price + 10000);
@@ -27,22 +28,24 @@ public class Television {
             System.out.println(i);
             System.out.println(bonus);
         }
-       //System.out.println(i);
+        //System.out.println(i);
     }
-    public void powerOff(){
+
+    public void powerOff() {
         System.out.println("tv를 끄다.");
     }
 
-    public int getPrice(){
+    public int getPrice() {
         return price;
     }
 
-    public int getPrice(int sale){
+    public int getPrice(int sale) {
         return price - sale;
     }
-    
+
     //자바는 메서드이름 동일해도 된다.
     //단, 입력값이 달라야함.
     //타입, 개수, 순서
+
 
 }
---
+++ day4/src/object/Test.java
@@ -0,0 +1,7 @@
+package object;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
