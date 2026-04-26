a6fb250147dee816a70fdcc0c0de1516202016dd
hera <hera@naver.com>
2026-04-26 18:30:15 +0900

    lib import

---
+++ day8-lib/src/test/LibTest.java
@@ -0,0 +1,13 @@
+package test;
+
+public class LibTest {
+    public static void main(String[] args) {
+        Cal cal = new Cal();
+        cal.add(100, 200);
+        Cal2 cal2 = new Cal2();
+        cal2.mul(100, 200);
+    }
+}
+
+//300
+//20000
