ab2dce628664d0bb2113b612cec4f3a3d42ce3a0
hera <hera@naver.com>
2026-04-26 19:21:53 +0900

    thread test

--- day8-lib/src/test/ThreadRun.java
+++ day8-lib/src/test/ThreadRun.java
@@ -37,9 +37,49 @@ public class ThreadRun extends JFrame {
         lblNewLabel_1.setFont(new Font("굴림", Font.BOLD, 35));
         getContentPane().add(lblNewLabel_1, BorderLayout.EAST);
 
-        CounterThread2 counter = new CounterThread2();
-        TimerThread2 timer = new TimerThread2();
-        ImgThread2 img = new ImgThread2();
+        Thread counter = new Thread(){
+            @Override
+            public void run() {
+                for (int i = 500; i > 0; i--) {
+                    top.setText("Counter: " + i);
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        Thread timer = new Thread(){
+            @Override
+            public void run() {
+                for (int i = 0; i < 300; i++) {
+                    Date date = new Date();
+                    sub.setText(String.valueOf(date));
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        Thread img = new Thread(){
+            @Override
+            public void run() {
+                String[] img = {"1.png", "2.png", "3.png", "4.png", "5.png"};
+                for (int i = 0; i < img.length; i++) {
+                    //center.setText("이미지: " + img[i]);
+                    ImageIcon icon = new ImageIcon(img[i]);
+                    center.setIcon(icon);
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
         counter.start();
         timer.start();
         img.start();
@@ -50,61 +90,7 @@ public class ThreadRun extends JFrame {
         setVisible(true);
     }
 
-    //내부클래스(inner class)
-    //변수를 다른 클래스와 공유할 목적
-    //단점: 내부클래스는 독립적으로 사용 불가
-    public class CounterThread2 extends Thread {
-        @Override
-        public void run() {
-            for (int i = 500; i > 0; i--) {
-                top.setText("Counter: " + i);
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    public class ImgThread2 extends Thread {
-        @Override //재정의
-        public void run() {
-            String[] img = {"1.png", "2.png", "3.png", "4.png", "5.png"};
-            for (int i = 0; i < img.length; i++) {
-//                center.setText("이미지: " + img[i]);
-                ImageIcon icon = new ImageIcon(img[i]);
-                center.setIcon(icon);
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    public class TimerThread2 extends Thread {
-        //@ => hera@naver.com(at, -에)
-        //@ => Annotation(표시, 표기) :기능없음.지워도됨
-        //    어노테이션
-        //@Component => 기능있음. new!객체생성
-        @Override //오버라이드, 재정의
-        public void run() {
-            for (int i = 0; i < 300; i++) {
-                Date date = new Date();
-                sub.setText(String.valueOf(date));
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
     public static void main(String[] args) {
         ThreadRun t = new ThreadRun();
     }
-
 }
