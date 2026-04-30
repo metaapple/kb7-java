db3b7f6c4752b137353fa22a8193bef4d1541d1a
metaapple <hera@naver.com>
2026-04-30 11:32:26 +0900

    collection

---
+++ day11-collection/src/collection/DebugTest.java
@@ -0,0 +1,12 @@
+package collection;
+
+public class DebugTest {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            sum += i;
+            //System.out.println(sum);
+        }
+        System.out.println(sum);
+    }
+}
